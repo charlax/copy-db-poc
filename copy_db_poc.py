77a9558bc15a9e29f8e504d903af5d1141b54074
Charles-Axel Dein <120501+charlax@users.noreply.github.com>
2021-10-14 17:43:00 +0200

    Remove default values

--- copy_db_poc.py
+++ copy_db_poc.py
@@ -1,27 +1,25 @@
 #!/usr/bin/env python3
 import argparse
 import copy
+import os
 import sys
 import traceback
 from uuid import uuid4
-from typing import cast
 
 import sqlalchemy
-from sqlalchemy import create_engine, select, func, event
+from sqlalchemy import create_engine, select, event
 from sqlalchemy import Table, Column, Integer, String, MetaData
+from sqlalchemy.engine import Engine
 
 TABLE_PREFIX = "dbin_"
 DEFAULT_BATCH_SIZE = 1000
 
-in_engine = create_engine(
-    "postgresql+psycopg2://user:password@127.0.0.1:5432/dbin", echo=True
-)
-out_engine = create_engine(
-    "mysql+mysqldb://user:password@127.0.0.1:3306/dbout", echo=True
-)
+DEFAULT_DB_IN = "postgresql+psycopg2://user:password@127.0.0.1:5432/dbin"
+DEFAULT_DB_OUT = "mysql+mysqldb://user:password@127.0.0.1:3306/dbout"
 
 
-def setup_fixtures():
+def setup_fixtures(in_engine: Engine) -> None:
+    """Install fixtures for testing."""
     metadata_in = MetaData()
     users = Table(
         "users",
@@ -52,8 +50,9 @@ def get_generic_type(type):
         new = type.as_generic()
 
         if isinstance(new, String) and not new.length:
-            # For MySQL
-            new.length = 500
+            # Length is required for VARCHAR
+            # (turns out we use the same length as Airbyte)
+            new.length = 512
 
         return new
 
@@ -64,6 +63,9 @@ def get_generic_type(type):
 
 def copy_table(
     table: Table,
+    *,
+    in_engine: Engine,
+    out_engine: Engine,
     batch_size: int = DEFAULT_BATCH_SIZE,
 ) -> None:
     """Copy a table."""
@@ -71,6 +73,8 @@ def copy_table(
     out_table.name = f"{TABLE_PREFIX}{table.name}"
     # Do not copy constraints
     out_table.constraints = set([])
+    # Do not copy indexes
+    out_table.indexes = set([])
 
     out_table.drop(out_engine, checkfirst=True)
     out_table.create(out_engine)
@@ -87,30 +91,42 @@ def copy_table(
                 conn_out.execute(ins)
 
 
-def copy_db():
+def copy_db(in_db_url: str, out_db_url: str) -> None:
     """Copy the db to its destination"""
+    in_engine = create_engine(in_db_url)
+    out_engine = create_engine(out_db_url)
+
     metadata = MetaData()
 
     @event.listens_for(metadata, "column_reflect")
-    def genericize_datatypes(inspector, tablename, column_dict):
+    def genericize_datatypes(inspector, table, column_dict):
         previously = column_dict["type"]
+        # No need for default value (such as nextval(...))
+        del column_dict["default"]
         column_dict["type"] = get_generic_type(previously)
 
     metadata.reflect(bind=in_engine)
 
     for t in reversed(metadata.sorted_tables):
-        copy_table(t)
+        copy_table(t, in_engine=in_engine, out_engine=out_engine)
 
 
-def main() -> int:
-    setup_fixtures()
+def main(should_install_fixtures: bool = False) -> int:
+    in_db_url = os.environ.get("DB_IN", DEFAULT_DB_IN)
+    out_db_url = os.environ.get("DB_OUT", DEFAULT_DB_OUT)
 
-    copy_db()
+    print(f"copy db from={in_db_url} to={out_db_url}")
+
+    if should_install_fixtures:
+        setup_fixtures(in_db_url=in_db_url)
+
+    copy_db(in_db_url=in_db_url, out_db_url=out_db_url)
     return 0
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="copy db over")
+    parser = argparse.ArgumentParser(description="copy db to destination")
+    parser.add_argument("--fixtures", action="store_true", help="install fixtures")
     args = parser.parse_args()
 
-    sys.exit(main())
+    sys.exit(main(should_install_fixtures=args.fixtures))
